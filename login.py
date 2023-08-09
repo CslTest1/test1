432d64515aed09c6d88f11734d82acb532a67b69
CslTest1 <141842517+CslTest1@users.noreply.github.com>
2023-08-09 10:49:51 -0300

    Create login.py

---
+++ login.py
@@ -0,0 +1,22 @@
+import requests
+import json
+
+URL = "https://ormus.com.br/api/v1/login"
+HEADERS = {
+  "Content-Type": "application/json"
+}
+
+
+def login(username, password):
+  credentials = {
+    "username": username,
+    "password": password
+  }
+  response = requests.post(url, data=json.dumps(credentials), headers=headers)
+  return reponse.json()
+
+
+if __name__ == '__main__':
+  username = "meu_mano"
+  password = "0rmus8121"
+  print(login(username, password))
